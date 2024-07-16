88fde47a1104b9578726751f07c787731079f438
DavidCa25 <banerdavid29@gmail.com>
2024-07-16 11:25:56 -0600

    [feat] Se agregó para el backend los metodos para actualizar producto y eliminar Producto

--- Servidor/src/controllers/ProductoController.ts
+++ Servidor/src/controllers/ProductoController.ts
@@ -10,7 +10,7 @@ interface RegistroProductoRequestBody{
     categoria: string,
     precio: number,
     cantidad: number,
-    fechaCreacion: Date,
+    fechaCreacion?: Date,
     fechaActualizacion: Date
 }
 
@@ -20,6 +20,8 @@ export default class ProductoController extends Controller{
     }
     protected initializeRouter(): void {
         this.router.post('/addProducto', this.registrarProducto)
+        this.router.put('/updateProducto/:id', this.actualizarProducto)
+        this.router.delete('/deleteProducto/:id', this.eliminar)
     }
 
     private async registrarProducto(req: Request, res: Response): Promise<void>{
@@ -46,7 +48,7 @@ export default class ProductoController extends Controller{
                 
             });
         }catch(e){
-            if (e instanceof Error && e.message === 'ErrorCodigoUsuarioDuplicado') {
+            if (e instanceof Error && e.message === 'ErrorCodigoProductoDuplicado') {
                 res.status(HttpStatusCodes.CONFLICT).json({
                   mensaje: 'Ya existe un producto con el mismo código.',
                 });
@@ -56,4 +58,55 @@ export default class ProductoController extends Controller{
             res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).end();
         }
     }
+
+    private async actualizarProducto(req: Request, res: Response): Promise<void>{
+    try{
+        const{codigo, producto, descripcion, categoria, precio, cantidad} = <RegistroProductoRequestBody>req.body
+
+        if(!codigo || !producto || !descripcion || !categoria || !precio || !cantidad){
+            res.status(HttpStatusCodes.BAD_REQUEST).end();
+            return
+        }
+
+        const id = parseInt(req.params.id);
+
+        const updateProduct = await Producto.buscarPorId(id);
+        
+        await updateProduct.actualizarProducto(codigo, producto, descripcion, categoria, precio, cantidad);
+        res.status(HttpStatusCodes.OK).json({
+            mensaje: 'Producto actualizado correctamente.',
+            producto: updateProduct 
+        });
+
+        
+        } catch (e) {
+        if (e instanceof Error && e.message === 'ErrorProductoNoEncontrado') {
+            res.status(HttpStatusCodes.NOT_FOUND).end();
+            return;
+        }
+
+        if (e instanceof Error && e.message === 'ErrorProductoDuplicado') {
+            res
+            .status(HttpStatusCodes.CONFLICT)
+            .json({ mensaje: 'Ya existe un producto con el mismo código.' });
+            return;
+        }
+
+        console.error(e);
+        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).end();
+        }
+    }
+
+    private async eliminar(req: Request, res: Response): Promise<void> {
+        try {
+          const id = parseInt(req.params.id);
+    
+          const producto = await Producto.eliminar(id);
+    
+          res.status(HttpStatusCodes.OK).json(producto);
+        } catch (e) {
+          console.error(e);
+          res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).end();
+        }
+      }
 }
--- Servidor/src/models/entities/Producto.ts
+++ Servidor/src/models/entities/Producto.ts
@@ -15,29 +15,29 @@ import {
     @PrimaryGeneratedColumn()
     public idProducto: number;
   
-    @Column({ type: 'varchar', length: 150 })
+    @Column({ type: 'varchar', length: 150, unsigned: true, unique: true})
     @Index()
     public codigo: string;
   
-    @Column({ type: 'varchar', length: 150 })
+    @Column({ type: 'varchar', length: 150, nullable: true })
     @Index()
     public producto: string;
   
     @Column({ type: 'varchar', length: 150, nullable: true })
     public descripcion: string;
   
-    @Column({ type: 'varchar', length: 50 })
+    @Column({ type: 'varchar', length: 50, nullable: true })
     @Index()
-    public categoria: string;
+    public categoria: string; 
   
-    @Column({ type: 'int' })
+    @Column({ type: 'int', nullable: true })
     public precio: number;
   
-    @Column({ type: 'int' })
+    @Column({ type: 'int', nullable: true })
     public cantidad: number;
   
     @CreateDateColumn()
-    public fechaCreacion: Date;
+    public fechaCreacion?: Date;
   
     @UpdateDateColumn()
     public fechaActualizacion: Date;
@@ -49,9 +49,9 @@ import {
       descripcion: string | null,
       categoria: string,
       precio: number,
-      cantidad: number,
-      fechaCreacion: Date,
-      fechaActualizacion: Date
+      cantidad: number,    
+      fechaActualizacion: Date,
+      fechaCreacion?: Date,
     ) {
       this.idProducto = <number>idProducto;
       this.codigo = codigo;
@@ -70,7 +70,7 @@ import {
       descripcion: string,
       categoria: string,
       precio: number,
-      cantidad: number,
+      cantidad: number
     ): Promise<Producto>{
       const repositorioProducto = await this.obtenerRepositorioProductos();
       const fechaCreacion = new Date();
@@ -98,9 +98,57 @@ import {
       return newProducto
     }
     
+
+    public async actualizarProducto(
+      codigo: string, 
+      producto: string,
+      descripcion: string,
+      categoria: string,
+      precio: number,
+      cantidad: number
+    ): Promise<void> {
+      
+      this.codigo = codigo;
+      this.producto = producto;
+      this.descripcion = descripcion;
+      this.categoria = categoria;
+      this.precio = precio;
+      this.cantidad = cantidad;
+      this.fechaActualizacion = new Date();
+      const repositorioProducto = await Producto.obtenerRepositorioProductos();
+
+      try {
+        await repositorioProducto.save(this);
+      } catch (e) {
+        if (e instanceof QueryFailedError && e.message.includes('ER_DUP_ENTRY')) {
+          throw new Error('ErrorProductoDuplicado');
+        }
+        throw e;
+      }
+    }
+    
+
+    public static async buscarPorId(idProducto: number): Promise<Producto> {
+      const repositorioUsuarios = await Producto.obtenerRepositorioProductos();
+  
+      const producto = await repositorioUsuarios.findOneBy({ idProducto });
+  
+      if (!producto) {
+        throw new Error('ErrorProductoNoEncontrado');
+      }
+  
+      return producto;
+    }
+    
     private static async obtenerRepositorioProductos(): Promise<Repository<Producto>> {
     const databaseConnection = await DatabaseConnection.getConnectedInstance();
     return databaseConnection.getRepository(Producto);
-  }
+    }
+
+    public static async eliminar(id: number): Promise<void> {
+      const repositorioUsuarios = await Producto.obtenerRepositorioProductos();
+      await repositorioUsuarios.delete(id);
+    }
+  
   }
   
