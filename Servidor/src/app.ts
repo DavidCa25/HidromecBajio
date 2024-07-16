131ca39589d748eb0d9695f5c033dc758a1822a5
DavidCa25 <banerdavid29@gmail.com>
2024-07-16 17:07:42 -0600

    [feat] Se agregó la funcionalidad para actualizar Inventario, obtenerInventario, se eliminó campo Venta, Se agregó controller en App.ts

--- Servidor/src/app.ts
+++ Servidor/src/app.ts
@@ -1,5 +1,6 @@
 
 import AppBuilder from './appBuilder';
+import InventarioController from './controllers/InventarioController';
 import ProductoController from './controllers/ProductoController';
 import UsuarioController from './controllers/UsuarioController';
 
@@ -10,9 +11,11 @@ appBuilder.withJsonBodyParser();
 
 const usuarioController = new UsuarioController();
 const productoController = new ProductoController();
+const inventarioController = new InventarioController();
 
 appBuilder.registerController(usuarioController);
 appBuilder.registerController(productoController);
+appBuilder.registerController(inventarioController);
 
 const app = appBuilder.getApp();
 
